9f15a2dfaeee7ed8fa406bd82b4861c3029bbb49
Congyuan Tang <tangcongyuan@gmail.com>
2018-12-05 07:13:24 +0000

    Modified bazel build options. Added TENSORFLOW_USE_EPU macro for compiling.

--- flags.sh
+++ flags.sh
@@ -1,8 +1,7 @@
 #compile_flags="--config=mkl --config=opt --verbose_failures"
 #compile_flags="--compilation_mode=dbg --verbose_failures"
 #compile_flags="--config cuda --compilation_mode=dbg --verbose_failures"
-#compile_flags="--config=opt --cxxopt=-DTENSORFLOW_USE_EPU"
-compile_flags="--config=opt"
+compile_flags="--config=opt --cxxopt=-DTENSORFLOW_USE_EPU"
 
 #testcases="//tensorflow/python:mock_ip_test //tensorflow/core:fpga_util_test //tensorflow/python/kernel_tests:fpga_corex_ip_ops_test" #//tensorflow/core/kernels:fpga_pooling_op_test
 #testcases="//tensorflow/core:fpga_util_test" # //tensorflow/python/kernel_tests:fpga_corex_ip_ops_test" #//tensorflow/core/kernels:fpga_pooling_op_test
